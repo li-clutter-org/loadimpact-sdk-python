4ba46ac6674f7972d70d2e4f819303e38a934462
Robin Gustafsson <robin@loadimpact.com>
2013-10-17 00:03:30 +0200

    Add license notice and update description.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 # coding=utf-8
 
+"""
+Copyright 2013 Load Impact
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 # Make sure setup tools is installed, if not install it.
 from ez_setup import use_setuptools
 use_setuptools()
@@ -18,7 +34,7 @@ setup(
     packages=['loadimpact'],
     url='http://developers.loadimpact.com/',
     license='LICENSE.txt',
-    description="The Load Impact API SDK provides Python APIs to create and manage load tests",
+    description="The Load Impact SDK provides access to Load Impact's cloud-based load testing platform",
     install_requires=['requests'],
     classifiers=[
         'Intended Audience :: Developers',
